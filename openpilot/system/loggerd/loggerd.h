01e0fe3a92df44d31c047f933a2ba9392cc7bb39
Rick Lan <ricklan@gmail.com>
2026-09-18 01:04:19 +0800

    loggerd: livestream size for tici

--- openpilot/system/loggerd/loggerd.h
+++ openpilot/system/loggerd/loggerd.h
@@ -28,6 +28,10 @@ const int SEGMENT_LENGTH = LOGGERD_TEST ? atoi(getenv("LOGGERD_SEGMENT_LENGTH"))
 
 inline int livestream_width() {
   switch (Hardware::get_device_type()) {
+    // dp - tici shares tizi's 1928x1208 camera (camera.py maps ar0231/ox03c10 to the
+    // same config for both), so it takes the same downscale. Upstream has no TICI case
+    // only because 0.11.2 dropped tici from device_map entirely.
+    case cereal::InitData::DeviceType::TICI:
     case cereal::InitData::DeviceType::TIZI: return 1152;
     case cereal::InitData::DeviceType::MICI: return 1280;
     default: return -1;
@@ -36,6 +40,7 @@ inline int livestream_width() {
 
 inline int livestream_height() {
   switch (Hardware::get_device_type()) {
+    case cereal::InitData::DeviceType::TICI:
     case cereal::InitData::DeviceType::TIZI:
     case cereal::InitData::DeviceType::MICI: return 720;
     default: return -1;
